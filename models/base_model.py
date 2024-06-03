411f351632f95508917c25fd69c22a613e1e11b7
Momen Sameh <momensameh5@gmail.com>
2024-06-03 09:57:37 -0700

    changed create and base for 1st task

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,52 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
+
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = self.cnvrt_to_dt(kwargs['updated_at'])
+            else:
+                self.updated_at = datetime.now()
+
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = self.cnvrt_to_dt(kwargs['created_at'])
+            else:
+                self.created_at = datetime.now()
+
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+
             self.__dict__.update(kwargs)
+
+        storage.new(self)
+
+    @staticmethod
+    def cnvrt_to_dt(str):
+        """Convert a string to a datetime object
+
+        Args:
+            str: String to convert
+
+        Returns:
+            datetime: Datetime object
+        """
+        return datetime.strptime(str, "%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -29,7 +56,6 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
