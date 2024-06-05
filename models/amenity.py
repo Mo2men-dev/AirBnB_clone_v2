2160639e41998ba816b4a941181bf9b96fb105b0
Momen Sameh <momensameh5@gmail.com>
2024-06-05 05:03:47 -0700

    fixing some bugs

--- models/amenity.py
+++ models/amenity.py
@@ -3,13 +3,16 @@
 import models
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """ The Amenity class """
     if models.storage_type == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       viewonly=False)
     else:
         name = ""
 
