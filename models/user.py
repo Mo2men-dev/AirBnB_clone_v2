be138fa1ebe2bbac2041c39f74291358d75c36b0
Momen Sameh <momensameh5@gmail.com>
2024-06-05 03:05:16 -0700

    added the table name attr

--- models/user.py
+++ models/user.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = "users"
     if models.storage_type == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
