5e283b06d413e78069e1c416122e23a214a14b9b
Momen Sameh <momensameh5@gmail.com>
2024-06-05 04:37:03 -0700

    update to the review, user and place models

--- models/place.py
+++ models/place.py
@@ -19,6 +19,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place")
 
     else:
         city_id = ""
@@ -36,3 +37,15 @@ class Place(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """Inits the place class"""
         super().__init__(*args, **kwargs)
+
+    if models.storage_t != 'db':
+        @property
+        def reviews(self):
+            """getter attribute returns the list of Review instances"""
+            from models.review import Review
+            review_list = []
+            all_reviews = models.storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
